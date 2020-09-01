5db3ded733bcccffd73f956a3fb5428b10dbfc2f
d.bystrikov <zelezodoroznik@yandex.ru>
2020-09-01 11:02:19 +0300

    dz 6

--- lesson14/dz/conftest.py
+++ lesson14/dz/conftest.py
@@ -1,9 +1,26 @@
 import pytest
 import logging
+import allure
+import requests
+import time
 from selenium import webdriver
 from selenium.webdriver.support.events import EventFiringWebDriver, AbstractEventListener
 
 logging.basicConfig(level=logging.INFO, filename="C:\\PycharmProjects\\OtusPythonQAJune2020\\lesson14\\dz\\results\\selenium.log")
+
+
+@allure.step("Жду доступности видео")
+def url_data_exists(url):
+    # Ждем доступности url
+    wait = 15
+    while wait > 0:
+        r = requests.get(url)
+        if r.ok:
+            return True
+        else:
+            time.sleep(1)
+            wait -= 1
+    return False
 
 
 class MyListener(AbstractEventListener):
@@ -32,17 +49,24 @@ def browser_se(request):
     caps = {"browserName": browser,
             #"version": "83.0",
             "enableVnc": True,
-            #"enableVideo": True,
-            # "enableLog": True,
+#            "enableVideo": True,
+            "enableLog": True,
             "screenResolution": "1280x720",
             "name": request.node.name}
 
     driver = webdriver.Remote(command_executor=executor_url, desired_capabilities=caps, options=options)
 
+    session_id = driver.session_id
+
+    allure.attach(name=driver.session_id,
+                  body=str(driver.desired_capabilities),
+                  attachment_type=allure.attachment_type.JSON)
+
     driver = EventFiringWebDriver(driver, MyListener())
     fixture_logger.info(f"Start session {driver.session_id}")
     request.addfinalizer(driver.quit)
     return driver
+
 
 @pytest.fixture
 def url_browser_se(request):
--- lesson14/dz/test/test_pageobject_opencart.py
+++ lesson14/dz/test/test_pageobject_opencart.py
@@ -4,92 +4,130 @@ from lesson14.dz.page_objects.CatalogDesktops import CatalogDesktops
 from lesson14.dz.page_objects.ProductId_49 import ProductId49
 from lesson14.dz.page_objects.AccountLogin import AccountLogin
 from lesson14.dz.page_objects.AdminPage import AdminPage
+import allure
 
 
 class TestClassOpencart:
 
     # 1 тест
+    @allure.link('https://demo.opencart.com/')
+    @allure.severity(allure.severity_level.CRITICAL)
+    @allure.story('story')
+    @allure.feature('epic')
+    @allure.title('main_page')
     def test_main_page(self, browser_se, url_browser_se):
         browser_se.get(url_browser_se)
         assert browser_se.title == 'Your Store'
-        element_search = MainPage(browser_se).element_search()
-        element_search.send_keys("Apple Cinema 30")
-        MainPage(browser_se).button_search()
-        assert browser_se.current_url == \
-               'https://demo.opencart.com/index.php?route=product/search&search=Apple%20Cinema%2030'
-        assert browser_se.title == 'Search - Apple Cinema 30'
-        BasePage(browser_se).return_home()
-        assert browser_se.title == 'Your Store'
-        MainPage(browser_se).button_dropdown()
-        assert MainPage(browser_se).navbar_in_home_page() == \
-               'Desktops\nLaptops & Notebooks\nComponents\nTablets\nSoftware\nPhones & PDAs\nCameras\nMP3 Players'
-        assert 'MacBook' in MainPage(browser_se).featured_in_home_page()
-        MainPage(browser_se).bullet()
-        assert MainPage(browser_se).img_in_main_page() == \
+        with allure.step("На главной странице ищу продукт: Apple Cinema 30"):
+            element_search = MainPage(browser_se).element_search()
+            element_search.send_keys("Apple Cinema 30")
+            MainPage(browser_se).button_search()
+            assert browser_se.current_url == \
+                'https://demo.opencart.com/index.php?route=product/search&search=Apple%20Cinema%2030'
+            assert browser_se.title == 'Search - Apple Cinema 30'
+        with allure.step("Возвращаюсь обратно на главную страницу"):
+            BasePage(browser_se).return_home()
+            assert browser_se.title == 'Your Store'
+        with allure.step("Проверяю, что на главной странице есть меню с выбором категорий"):
+            MainPage(browser_se).button_dropdown()
+            assert MainPage(browser_se).navbar_in_home_page() == \
+                'Desktops\nLaptops & Notebooks\nComponents\nTablets\nSoftware\nPhones & PDAs\nCameras\nMP3 Players'
+        with allure.step("Проверяею, что есть MacBook в меню категорий"):
+            assert 'MacBook' in MainPage(browser_se).featured_in_home_page()
+        with allure.step("Смотрю, что в рекламном отсеке есть нужная фотка"):
+            MainPage(browser_se).bullet()
+            assert MainPage(browser_se).img_in_main_page() == \
                 'https://demo.opencart.com/image/cache/catalog/demo/banners/MacBookAir-1140x380.jpg'
 
     # 2 тест
+    @allure.link('https://demo.opencart.com/index.php?route=product/category&path=20')
+    @allure.severity(allure.severity_level.NORMAL)
+    @allure.title('catalog_desktops')
     def test_catalog_desktops(self, browser_se, url_browser_se):
         browser_se.get(f'{url_browser_se}/index.php?route=product/category&path=20')
-        assert browser_se.title == 'Desktops'
-        assert CatalogDesktops(browser_se).img_thumbnail() == \
-               'https://demo.opencart.com/image/cache/catalog/demo/compaq_presario-80x80.jpg'
-        CatalogDesktops(browser_se).refine_search_pc()
-        assert browser_se.current_url == 'https://demo.opencart.com/index.php?route=product/category&path=20_26'
-        BasePage(browser_se).return_desktop()
-        assert browser_se.title == 'Desktops'
-        CatalogDesktops(browser_se).refine_search_mac()
-        assert browser_se.current_url == 'https://demo.opencart.com/index.php?route=product/category&path=20_27'
-        BasePage(browser_se).return_desktop()
-        assert browser_se.title == 'Desktops'
+        with allure.step("Смотрю, что открыто меню Desktops и есть картинка"):
+            assert browser_se.title == 'Desktops'
+            assert CatalogDesktops(browser_se).img_thumbnail() == \
+                'https://demo.opencart.com/image/cache/catalog/demo/compaq_presario-80x80.jpg'
+        with allure.step("Смотрю, что каталог ПК кликабельный"):
+            CatalogDesktops(browser_se).refine_search_pc()
+            assert browser_se.current_url == 'https://demo.opencart.com/index.php?route=product/category&path=20_26'
+        with allure.step("возвращаюсь в меню Desktop"):
+            BasePage(browser_se).return_desktop()
+            assert browser_se.title == 'Desktops'
+        with allure.step("Смотрю, что каталог МАК кликабельный"):
+            CatalogDesktops(browser_se).refine_search_mac()
+            assert browser_se.current_url == 'https://demo.opencart.com/index.php?route=product/category&path=20_27'
+        with allure.step("возвращаюсь в меню Desktop"):
+            BasePage(browser_se).return_desktop()
+            assert browser_se.title == 'Desktops'
         assert "Laptops & Notebooks (5)" in CatalogDesktops(browser_se).desktop_colomn()
 
     # 3 тест
+    @allure.link('https://demo.opencart.com/index.php?route=product/product&path=57&product_id=49')
+    @allure.severity(allure.severity_level.NORMAL)
+    @allure.title('product_id_49')
     def test_product_id_49(self, browser_se, url_browser_se):
-        browser_se.get(f'{url_browser_se}/index.php?route=product/product&path=57&product_id=49')
-        assert browser_se.title == 'Samsung Galaxy Tab 10.1'
-        ProductId49(browser_se).wish_in_product()
-        assert ProductId49(browser_se).alert_wish_login() == \
-               'You must login or create an account to save Samsung Galaxy Tab 10.1 to your wish list!\n×'
-        ProductId49(browser_se).compare()
-        assert ProductId49(browser_se).alert_success() == \
-               'Success: You have added Samsung Galaxy Tab 10.1 to your product comparison!\n×'
-        assert ProductId49(browser_se).thumbnail() == \
-               'https://demo.opencart.com/image/cache/catalog/demo/samsung_tab_1-500x500.jpg'
-        assert ProductId49(browser_se).price_product()[8].text == '$241.99\nEx Tax: $199.99'
+        with allure.step("Проверяем , что товар под айди 49 Самсунг"):
+            browser_se.get(f'{url_browser_se}/index.php?route=product/product&path=57&product_id=49')
+            assert browser_se.title == 'Samsung Galaxy Tab 10.1'
+        with allure.step("Добавляем Самсунг в список любимых продуктов"):
+            ProductId49(browser_se).wish_in_product()
+            assert ProductId49(browser_se).alert_wish_login() == \
+                'You must login or create an account to save Samsung Galaxy Tab 10.1 to your wish list!\n×'
+        with allure.step("добавляем Самсунг в список сравнения"):
+            ProductId49(browser_se).compare()
+            assert ProductId49(browser_se).alert_success() == \
+                'Success: You have added Samsung Galaxy Tab 10.1 to your product comparison!\n×'
+        with allure.step("Проверяем наличие фотки и стоимости"):
+            assert ProductId49(browser_se).thumbnail() == \
+                'https://demo.opencart.com/image/cache/catalog/demo/samsung_tab_1-500x500.jpg'
+            assert ProductId49(browser_se).price_product()[8].text == '$241.99\nEx Tax: $199.99'
 
     # 4 тест
+    @allure.link('https://demo.opencart.com/index.php?route=account/login')
+    @allure.severity(allure.severity_level.TRIVIAL)
+    @allure.title('account')
     def test_account(self, browser_se, url_browser_se):
         browser_se.get(f'{url_browser_se}/index.php?route=account/login')
-        assert browser_se.title == 'Account Login'
-        AccountLogin(browser_se) \
-            .login_user(email="fdgfd@gff.com", password="5555")
-        assert browser_se.title == 'My Account'
-        AccountLogin(browser_se) \
-            .logout() \
-            .continue_click()
-        assert browser_se.title == 'Your Store'
-        MainPage(browser_se) \
-            .my_account() \
-            .menu_my_account()
-        AccountLogin(browser_se) \
-            .login_user(email="fdgfd@gff.com", password="5555") \
-            .wish_list()
+        with allure.step("Вводим логин и пароль"):
+            assert browser_se.title == 'Account Login'
+            AccountLogin(browser_se) \
+                .login_user(email="fdgfd@gff.com", password="5555")
+            assert browser_se.title == 'My Account'
+        with allure.step("делаем раздлгин"):
+            AccountLogin(browser_se) \
+                .logout() \
+                .continue_click()
+            assert browser_se.title == 'Your Store'
+        with allure.step("Делаем логин"):
+            MainPage(browser_se) \
+                .my_account() \
+                .menu_my_account()
+            AccountLogin(browser_se) \
+                .login_user(email="fdgfd@gff.com", password="5555") \
+                .wish_list()
         assert browser_se.title == 'My Wish List'
         assert AccountLogin(browser_se).product_name_in_wish_list() \
                == 'My Wish List\nImage Product Name Model Stock Unit Price Action' \
                   '\nSamsung Galaxy Tab 10.1 SAM1 Pre-Order\n$199.99\nContinue'
 
     # 5 тест
+    @allure.link('https://demo.opencart.com/admin/')
+    @allure.severity(allure.severity_level.NORMAL)
+    @allure.title('admin')
     def test_admin(self, browser_se, url_browser_se):
         browser_se.get(f'{url_browser_se}/admin/')
-        AdminPage(browser_se).button_login()
-        assert browser_se.title == 'Dashboard'
-        AdminPage(browser_se).button_logout()
-        assert browser_se.title == 'Administration'
-        AdminPage(browser_se) \
-            .button_login() \
-            .catalogs() \
-            .products()
-        assert browser_se.title == "Products"
-        assert "Apple Cinema 30" in AdminPage(browser_se).table()
+        with allure.step("Делаем логин"):
+            AdminPage(browser_se).button_login()
+            assert browser_se.title == 'Dashboard'
+        with allure.step("Делаем разлогин"):
+            AdminPage(browser_se).button_logout()
+            assert browser_se.title == 'Administration'
+        with allure.step("Переходим в таблицу и ищём Applq"):
+            AdminPage(browser_se) \
+                .button_login() \
+                .catalogs() \
+                .products()
+            assert browser_se.title == "Products"
+            assert "Apple Cinema 30" in AdminPage(browser_se).table()
